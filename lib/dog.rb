c53685b37891e8b66adfede1db8c38d483d0a40a
Ming Duan <11203duan@gmail.com>
2019-02-28 06:53:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+def name=(dog_na)
+  @name = dog_na
+end
+
+def name
+  @name
+end
+
+def breed=(dog_br)
+  @breed = dog_br
+end
+
+def breed
+  @breed
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(input)
+    @name = input
+  end
+
+  def name
+    @name
+  end
+
+  def job=(input2)
+    @job = input2
+  end
+
+  def job
+    @job
+  end
+
+end
